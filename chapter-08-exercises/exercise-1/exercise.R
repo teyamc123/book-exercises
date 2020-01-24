c5260d11c82c64c3cfa0669e093c16fd2bf91414
teyamc123 <teyamc@uw.edu>
2020-01-23 23:17:36 -0800

    completed Chapter 8 and Chapter 10 exercises

--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -1,40 +1,50 @@
 # Exercise 1: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
-
+my_breakfast <- c("eggs", "sausage", "hashbrowns")
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
-
+my_lunch <- c("grilled cheese", "tomato soup")
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+meals <- list(breakfast = my_breakfast, lunch = my_lunch)
+print(meals)
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
-
+meals$dinner <- c("mac and cheese", "veggie soup")
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
+dinnner <- meals$dinner
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-
+meals[[5]] <- meals[["lunch"]]
+print(meals)
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
-
+early_meals <- meals[c("breakfast", "lunch")]
+print(early_meals)
 
 
 ### Challenge ###
 
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function to apply the `length()` function to each item
+num_of_breakfast <- length(meals$breakfast)
 
+  #this says apply on this object this function, so it applies the length to every object in meals
+lapply(meals, length)
 
 # Write a function `add_pizza` that adds pizza to a given meal vector, and
 # returns the pizza-fied vector
-
+add_pizza <- function(meal_vector){
+  new_vector <- c(meal_vector, "pizza")
+    
+  return(new_vector)
+}
 
 # Create a vector `better_meals` that is all your meals, but with pizza!
-
+better_meals <- lapply(meals, add_pizza)
